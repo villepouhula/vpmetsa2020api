0464f75ec9451471061bd9107109c817b3925295
moveniumville <ville.pouhula@movenium.com>
2017-03-01 11:57:42 +0200

    some changes

--- app/models/location.js
+++ app/models/location.js
@@ -7,7 +7,8 @@ var LocationSchema   = new Schema({
     loc: {
         type: [Number],  // [<longitude>, <latitude>]
         index: '2dsphere'      // create the geospatial index
-    }
+    },
+    timestamps: { createdAt: 'created_at', updatedAt: 'updated_at' }
 });
 
 module.exports = mongoose.model('Location', LocationSchema);
